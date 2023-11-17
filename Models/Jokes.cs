bebb9891f689d8d5f20ed468c5a1c76b99c24697
Martins-O <jojololamartins686@gmail.com>
2023-11-17 11:50:20 +0100

    just commit

---
+++ Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace JokeWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokesQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
---
+++ dbo.Jokes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Jokes] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [JokesQuestion] NVARCHAR (MAX) NULL,
+    [JokeAnswer]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Jokes] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
