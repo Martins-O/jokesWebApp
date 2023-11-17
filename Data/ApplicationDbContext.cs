1bad810914c33c202a5edf4bb4b4bec3d2c0c874
Martins-O <jojololamartins686@gmail.com>
2023-11-17 11:20:41 +0100

    commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApp.Models;
+
+namespace JokeWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokeWebApp.Models.Jokes> Jokes { get; set; } = default!;
+    }
+}
---
+++ Data/Migrations/20231117072404_intialsetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokeWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class intialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jokes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JokesQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jokes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jokes");
+        }
+    }
+}
